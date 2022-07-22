544b024b398906464d90c183a86f33d3a64f2d2a
hansaeging <93020146+hansaeging@users.noreply.github.com>
2022-07-22 17:31:34 +0900

    기본 templete 생성

--- website/auth.py
+++ website/auth.py
@@ -1,15 +1,15 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 auth = Blueprint('auth', __name__)
 
 @auth.route('/sign-in')
 def sign_in():
-    return "<p>Sign-In</p>"
+    return render_template('sign_in.html')
 
 @auth.route('/logout')
 def logout():
-    return "<p>Logout</p>"
+    return render_template('logout.html')
 
 @auth.route('/sign-up')
 def sign_up():
-    return "<p>Sign-Up</p>"
+    return render_template('sign_up.html')
